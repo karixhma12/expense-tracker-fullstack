446bcc4efe28c217e85d1780e2ab01777926c5c8
Karishma <karishma.7022@gmail.com>
2026-06-14 17:10:20 +0530

    feat: login and signup pages with auth context

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,26 @@
-
-
+import {useState} from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useAuth } from "../context/AuthContext";
 
 function Login(){
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const navigate = useNavigate();
+    const {login : saveToken} = useAuth();
+
+    async function login(){
+        const response = await axios.post("http://localhost:3000/api/auth/login",{email,password});
+        const token = response.data.token ; 
+        saveToken(token);
+        navigate("/dashboard");
+    }
+
     return(
         <div>
-            This is a login page
+            <input placeholder="enter your email.." value={email} type="text" onChange={(e)=>{setEmail(e.target.value)}}></input>
+            <input placeholder="enter your password.." value={password} type="text" onChange={(e)=>{setPassword(e.target.value)}}></input>
+            <button onClick={login}> Submit </button>
         </div>
     )
 }
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,9 +1,24 @@
-
+import {useState} from "react";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 function Signup(){
+    const [username,setUsername] = useState("");
+    const [password,setPassword] = useState("");
+    const [email,setEmail] = useState("");
+    const navigate = useNavigate();
+
+    async function signup(){
+        const response = await axios.post("http://localhost:3000/api/auth/signup",{username,email,password});
+        navigate("/login");
+    }
+
     return(
         <div>
-            This is a signup page
+            <input placeholder="enter your username.." type="text" value={username} onChange={(e)=>{setUsername(e.target.value)}}></input>
+            <input placeholder="enter your email-id.." type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}}></input>
+            <input placeholder="enter your password.." type="text" value={password} onChange={(e)=>{setPassword(e.target.value)}}></input>
+            <button onClick={signup}> Submit </button>
         </div>
     )
 }
